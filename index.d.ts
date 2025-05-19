1aacfd6db4d63e9ff8036bec8be0de0e041d76ae
cuongtruong@topdev.vn <cuongtruong@topdev.vn>
2025-05-19 11:46:56 +0700

    update code

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="react" />
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: 'primary' | 'secondary';
+    initialValue?: number;
+    onValueChange?: (value: number) => void;
 }
 declare const Button: React.FC<ButtonProps>;
 
