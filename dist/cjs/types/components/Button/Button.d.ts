e8ac358a2a5dc6ed90c3f748d9c672a9cee01456
cuongtruong@topdev.vn <cuongtruong@topdev.vn>
2025-05-19 11:46:56 +0700

    update code

--- dist/cjs/types/components/Button/Button.d.ts
+++ dist/cjs/types/components/Button/Button.d.ts
@@ -2,6 +2,8 @@
 import './Button.css';
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: 'primary' | 'secondary';
+    initialValue?: number;
+    onValueChange?: (value: number) => void;
 }
 declare const Button: React.FC<ButtonProps>;
 export default Button;
