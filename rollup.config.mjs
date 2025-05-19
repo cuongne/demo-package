c2b719921dc422516433a5a72872f94475261f0d
cuongtruong@topdev.vn <cuongtruong@topdev.vn>
2025-05-19 10:32:16 +0700

    .

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,14 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
 import packageJson from './package.json' assert { type: 'json' };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import alias from '@rollup/plugin-alias';
 import json from '@rollup/plugin-json';
 import generatePackageJson from 'rollup-plugin-generate-package-json';
@@ -30,7 +35,11 @@ export default [
           {
             find: 'axios/lib/adapters/http',
             replacement: 'axios/lib/adapters/xhr' // dùng adapter browser
-          }
+          },
+          {
+            find: 'react',
+            replacement: path.resolve(__dirname, 'node_modules/react'),
+          },
         ]
       }),
       resolve({
@@ -64,6 +73,6 @@ export default [
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-    external: [/\.css$/,'react', 'react-dom'],
+    external: [/\.css$/, 'react', 'react-dom'],
   },
 ];
